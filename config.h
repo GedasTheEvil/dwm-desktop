6f98ab99a20f1bfbffe62fcf88d8e91a98e34ace
Gediminas Skucas <gediminas.skucas@nfq.lt>
2016-04-14 10:30:47 +0300

    A more pleasing color for background

--- config.h
+++ config.h
@@ -8,8 +8,8 @@ static const char dmenufont[] = "monospace:size=10";
 static const char normbordercolor[] = "#444444";
 static const char normbgcolor[] = "#222222";
 static const char normfgcolor[] = "#bbbbbb";
-static const char selbordercolor[] = "#008222";
-static const char selbgcolor[] = "#008222";
+static const char selbordercolor[] = "#5383d5";
+static const char selbgcolor[] = "#5383d5";
 static const char selfgcolor[] = "#eeeeee";
 static const unsigned int borderpx = 1;
 /* border pixel of windows */
