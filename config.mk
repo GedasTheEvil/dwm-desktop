55013143bad1ddfd797acc8b13b0a462764e3f3e
Gediminas Skucas <gediminas.skucas@gmail.com>
2018-12-20 17:18:02 +0200

    T480: Adding a work-around for the DWM color emoji crash

--- config.mk
+++ config.mk
@@ -1,7 +1,5 @@
 # dwm version
-VERSION = 6.1-gedas-1.2-thinkpad-T480
-
-# Customize below to fit your system
+VERSION = 6.1-gedas-1.3-thinkpad-T480
 
 # paths
 PREFIX = /usr/local
@@ -17,22 +15,16 @@ XINERAMAFLAGS = -DXINERAMA
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
 FREETYPEINC = /usr/include/freetype2
-# OpenBSD (uncomment)
-#FREETYPEINC = ${X11INC}/freetype2
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
 LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 
 # flags
-CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+CPPFLAGS = -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = -s ${LIBS}
-
-# Solaris
-#CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
-#LDFLAGS = ${LIBS}
 
 # compiler and linker
 CC = cc
