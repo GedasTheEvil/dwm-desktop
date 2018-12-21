0fa9d6d8e527e7876e7c048f2780706108f258cd
Gediminas Skucas <gediminas.skucas@gmail.com>
2018-12-21 16:42:26 +0200

    Revert "T480: Adding a work-around for the DWM"
    This reverts commit 55013143bad1ddfd797acc8b13b0a462764e3f3e.

--- config.mk
+++ config.mk
@@ -1,5 +1,7 @@
 # dwm version
-VERSION = 6.1-gedas-1.3-thinkpad-T480
+VERSION = 6.1-gedas-1.2-thinkpad-T480
+
+# Customize below to fit your system
 
 # paths
 PREFIX = /usr/local
@@ -15,16 +17,22 @@ XINERAMAFLAGS = -DXINERAMA
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
 FREETYPEINC = /usr/include/freetype2
+# OpenBSD (uncomment)
+#FREETYPEINC = ${X11INC}/freetype2
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
 LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 
 # flags
-CPPFLAGS = -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = -s ${LIBS}
+
+# Solaris
+#CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
+#LDFLAGS = ${LIBS}
 
 # compiler and linker
 CC = cc
